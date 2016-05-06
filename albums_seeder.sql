2eb8887422a41152fd11957ba6f65b41aa58fb70
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-06 10:03:17 -0500

    Albums data to enter in Albums table.

---
+++ albums_seeder.sql
@@ -0,0 +1,24 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Michael Jackson', 'Thriller', '1982', '48.1', 'Pop, rock');
+
+VALUES('AC/DC', 'Back in Black', '1980', '25.9', 'Hard rock');
+
+VALUES('Pink Floyd', 'The Dark Side of the Moon', '1973', '22.7', 'Progressive rock');
+
+VALUES('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'Soundtrack/R&B');
+
+VALUES('Meat Loaf', 'Bat Out of Hell', '1977', '20.6', 'Hard rock');
+
+VALUES('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock, soft rock');
+
+VALUES('Bee Gees', 'Saturday Night Fever', '1977', '19', 'Disco');
+
+VALUES('Fleetwood Mac', 'Rumours', '1977', '27.9', 'Soft rock');
+
+VALUES('Led Zeppelin', 'Led Zeppelin', '1971', '29.0', 'Hard rock');
+
+VALUES('Led Zeppelin', 'Led Zeppelin', '1971', '29.0', 'Hard rock');
+
+VALUES('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '13.1', 'Rock');
