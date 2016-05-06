63693204297a24d9161a54396078492e2d3cc9ae
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-06 10:46:50 -0500

    Refactored code so it only inserts once.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,24 +1,24 @@
 USE codeup_test_db;
 
+TRUNCATE albums;
+
 INSERT INTO albums (artist, name, release_date, sales, genre)
-VALUES('Michael Jackson', 'Thriller', '1982', '48.1', 'Pop, rock');
+	VALUES('Michael Jackson', 'Thriller', '1982', '48.1', 'Pop, rock'),
 
-VALUES('AC/DC', 'Back in Black', '1980', '25.9', 'Hard rock');
+		('AC/DC', 'Back in Black', '1980', '25.9', 'Hard rock'),
 
-VALUES('Pink Floyd', 'The Dark Side of the Moon', '1973', '22.7', 'Progressive rock');
+		('Pink Floyd', 'The Dark Side of the Moon', '1973', '22.7', 'Progressive rock'),
 
-VALUES('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'Soundtrack/R&B');
+		('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'Soundtrack/R&B'),
 
-VALUES('Meat Loaf', 'Bat Out of Hell', '1977', '20.6', 'Hard rock');
+		('Meat Loaf', 'Bat Out of Hell', '1977', '20.6', 'Hard rock'),
 
-VALUES('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock, soft rock');
+		('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock, soft rock'),
 
-VALUES('Bee Gees', 'Saturday Night Fever', '1977', '19', 'Disco');
+		('Bee Gees', 'Saturday Night Fever', '1977', '19', 'Disco'),
 
-VALUES('Fleetwood Mac', 'Rumours', '1977', '27.9', 'Soft rock');
+		('Fleetwood Mac', 'Rumours', '1977', '27.9', 'Soft rock'),
 
-VALUES('Led Zeppelin', 'Led Zeppelin', '1971', '29.0', 'Hard rock');
+		('Led Zeppelin', 'Led Zeppelin', '1971', '29.0', 'Hard rock'),
 
-VALUES('Led Zeppelin', 'Led Zeppelin', '1971', '29.0', 'Hard rock');
-
-VALUES('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '13.1', 'Rock');
+		('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '13.1', 'Rock');
