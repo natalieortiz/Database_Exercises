b9e7c099f512daa5b94734427227597f2e5afcb9
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-09 11:22:36 -0500

    Using distinct to find unique last names that have q but not qu.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -3,3 +3,9 @@ FROM titles;
 
 SELECT DISTINCT first_name, last_name FROM employees  
 WHERE last_name LIKE 'E%e';
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name LIKE '%q%'
+	AND (
+		NOT last_name LIKE '%qu%'
+		);
