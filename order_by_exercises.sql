3bbb767a318601bea81d97830f5ade14f875b596
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-09 10:33:01 -0500

    Adding order by to sql queries.

---
+++ order_by_exercises.sql
@@ -0,0 +1,31 @@
+SELECT first_name, last_name, gender FROM employees
+WHERE gender = 'M'
+	AND (
+	first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya' 
+	)
+	ORDER BY first_name ASC, last_name ASC;
+
+
+SELECT * FROM employees 
+WHERE last_name LIKE 'E%'
+	OR last_name LIKE '%e';
+	
+SELECT * FROM employees 
+WHERE last_name LIKE 'E%' 
+	AND last_name LIKE '%e';
+
+
+SELECT first_name, last_name, hire_date FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE hire_date LIKE '199%-%-%'
+	AND birth_date LIKE '%-12-25';
+
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE '%q%'
+	AND (
+		NOT last_name LIKE '%qu%'
+		);
