ba62c86f61e429e1b50a08ac1a5e4aa1fe151fe8
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-09 11:20:01 -0500

    Showing unique first and last name combinations for last names that start and end with the letter e.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,7 +1,5 @@
 SELECT DISTINCT title
 FROM titles;
 
-SELECT last_name FROM employees  
-WHERE last_name LIKE 'E%' 
-	AND last_name LIKE '%e'
-	GROUP BY last_name;
+SELECT DISTINCT first_name, last_name FROM employees  
+WHERE last_name LIKE 'E%e';
