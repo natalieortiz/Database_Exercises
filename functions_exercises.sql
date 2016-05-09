51115fd5688aebd38d30bf5e6c78f72deb2f5294
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-09 13:31:49 -0500

    Finding date between current date and hire date for third query.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -10,16 +10,8 @@ SELECT CONCAT(first_name,last_name) FROM employees
 WHERE last_name LIKE 'E%'
 	OR last_name LIKE '%e'
 	ORDER BY emp_no DESC;
-	
-SELECT * FROM employees 
-WHERE last_name LIKE 'E%' 
-	AND last_name LIKE '%e';
 
-
-SELECT first_name, last_name, hire_date FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
-
-SELECT first_name, last_name, birth_date, hire_date FROM employees
+SELECT DATEDIFF(curdate(), hire_date) , first_name, last_name, birth_date, hire_date FROM employees
 WHERE hire_date LIKE '199%-%-%'
 	AND birth_date LIKE '%-12-25'
 	ORDER BY hire_date DESC, birth_date ASC;
