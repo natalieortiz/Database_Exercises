9b4430a200a0ff7336cdf01e1decf9b8b9ce8fea
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-09 09:40:43 -0500

    Chaining more where clauses to sql queries.

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,12 +10,21 @@ WHERE gender = 'M'
 SELECT * FROM employees 
 WHERE last_name LIKE 'E%'
 	OR last_name LIKE '%e';
+	
+SELECT * FROM employees 
+WHERE last_name LIKE 'E%' 
+	AND last_name LIKE '%e';
+
 
 SELECT first_name, last_name, hire_date FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
 
-SELECT first_name, last_name, birth_date FROM employees
-WHERE birth_date LIKE '%-12-25';
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE hire_date LIKE '199%-%-%'
+	AND birth_date LIKE '%-12-25';
 
 SELECT first_name, last_name FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+	AND (
+		NOT last_name LIKE '%qu%'
+		);
