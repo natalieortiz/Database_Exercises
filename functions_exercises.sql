4acced4706b47dbe8bbab55fdcf98b87797b8f32
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-09 13:23:29 -0500

    Counting number of each gender in the first query.

---
+++ functions_exercises.sql
@@ -0,0 +1,31 @@
+SELECT COUNT(*) FROM employees
+WHERE
+	first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya' 
+	GROUP BY gender;
+
+
+SELECT * FROM employees 
+WHERE last_name LIKE 'E%'
+	OR last_name LIKE '%e'
+	ORDER BY emp_no DESC;
+	
+SELECT * FROM employees 
+WHERE last_name LIKE 'E%' 
+	AND last_name LIKE '%e';
+
+
+SELECT first_name, last_name, hire_date FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE hire_date LIKE '199%-%-%'
+	AND birth_date LIKE '%-12-25'
+	ORDER BY hire_date DESC, birth_date ASC;
+
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE '%q%'
+	AND (
+		NOT last_name LIKE '%qu%'
+		);
